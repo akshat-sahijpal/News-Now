94ec0d7dca1a690371aafd2786f8bfb32e3b171d
akshat-sahijpal <20akshatSahijpal01droid01@gmail.com>
2021-06-08 17:57:13 +0530

    update ver 1.1.2

--- app/src/main/java/com/akshatsahijpal/newsnow/ui/fragment/headlines/subFragment/technology/TechnologyNewsFragment.kt
+++ app/src/main/java/com/akshatsahijpal/newsnow/ui/fragment/headlines/subFragment/technology/TechnologyNewsFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akshatsahijpal.newsnow.adapter.NewsAdapter
 import com.akshatsahijpal.newsnow.databinding.FragmentTechnologyNewsBinding
@@ -36,6 +38,16 @@ class TechnologyNewsFragment : Fragment() {
             model.getRefinedData().observe(viewLifecycleOwner) {
                 GlobalScope.launch { adapter.submitData(it) }
             }
+        }
+        adapter.addLoadStateListener {
+            if (it.source.refresh is LoadState.Loading) {
+                _binding.shimmerFrameLayout.isVisible = true
+                _binding.shimmerFrameLayout.startShimmer()
+            } else {
+                _binding.shimmerFrameLayout.stopShimmer()
+                _binding.shimmerFrameLayout.isVisible = false
+            }
+            _binding.recyclerViewTi.isVisible = it.source.refresh is LoadState.NotLoading
         }
     }
 }
